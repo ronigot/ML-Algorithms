64889469db2433a1223101f53d12dca7ddff05a1
ronigot <80850421+ronigot@users.noreply.github.com>
2022-08-14 00:04:05 +0300

    Add files via upload

---
+++ Classification-Algorithms/ex2.py
@@ -0,0 +1,178 @@
+# Roni Gotlib 322805029
+import sys
+import numpy as np
+
+x_train, y_train, x_test, output_file = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+x_train_lines = np.loadtxt(x_train, delimiter=",")
+x_test_lines = np.loadtxt(x_test, delimiter=",")
+y_train_lines = np.loadtxt(y_train)
+
+
+# A function that returns the class with the most instances
+def maximum(class_0, class_1, class_2):
+    if (class_0 >= class_1) and (class_0 >= class_2):
+        return 0
+    elif (class_1 >= class_0) and (class_1 >= class_2):
+        return 1
+    return 2
+
+
+def min_max_normalization(vector_to_normalize, train_vector):
+    np_vector = np.array(train_vector)
+    normalized_vector = vector_to_normalize.copy()
+
+    for i, line in enumerate(vector_to_normalize):
+        for j, value in enumerate(line):
+            min_value = min(np_vector[:, j])
+            max_value = max(np_vector[:, j])
+            new_value = (value - min_value) / (max_value - min_value)
+            normalized_vector[i][j] = new_value
+    return normalized_vector
+
+
+# KNN Algorithm
+def knn():
+    k = 9
+    normalized_x_train = min_max_normalization(x_train_lines, x_train_lines)
+    normalized_x_test = min_max_normalization(x_test_lines, x_train_lines)
+    labels = []
+    for test in normalized_x_test:
+        dists_list = list()
+        index = 0
+        for train in normalized_x_train:
+            # euclidean distance between test point and train point
+            dist = np.linalg.norm(test - train)
+            dists_list.insert(len(dists_list), (index, dist))
+            index += 1
+
+        # sort by distances
+        dists_list.sort(key=lambda t: t[1])
+        # counters for the number of instances of each class
+        class_0, class_1, class_2 = 0, 0, 0
+        # k nearest neighbors
+        for i in range(k):
+            int_class = y_train_lines[dists_list[i][0]]
+            if int_class == 0:
+                class_0 += 1
+            elif int_class == 1:
+                class_1 += 1
+            else:
+                class_2 += 1
+
+        # the class with the most instances
+        label = maximum(class_0, class_1, class_2)
+        labels.insert(len(labels), label)
+    return labels
+
+
+def z_score_normalization(vector_to_normalize, train_vector):
+    np_vector = np.array(train_vector)
+    normalized_vector = vector_to_normalize.copy()
+
+    for i, line in enumerate(vector_to_normalize):
+        for j, value in enumerate(line):
+            mean_value = np.mean(np_vector[:, j])
+            stand_dev_value = np.std(np_vector[:, j])
+            new_value = (value - mean_value) / stand_dev_value
+            normalized_vector[i][j] = new_value
+
+    return normalized_vector
+
+
+# prediction with weights
+def predict(weights):
+    normalized_x_test = z_score_normalization(x_test_lines, x_train_lines)
+    normalized_x_test = np.insert(normalized_x_test, 0, np.ones(normalized_x_test.shape[0]), axis=1)
+    labels = []
+    for test in normalized_x_test:
+        y_hat = np.argmax(np.dot(weights, test))
+        labels.insert(len(labels), y_hat)
+    return labels
+
+
+# estimate Perceptron weights
+def train_perceptron():
+    normalized_x_train = z_score_normalization(x_train_lines, x_train_lines)
+    normalized_x_train = np.insert(normalized_x_train, 0, np.ones(normalized_x_train.shape[0]), axis=1)
+    epochs = 22
+    weights = [[0] * 6 for i in range(3)]
+    for i in range(epochs):
+        for x, y in zip(normalized_x_train, y_train_lines):
+            y_hat = np.argmax(np.dot(weights, x))
+            if y_hat != y:
+                weights[int(y)] += x * 0.1
+                weights[y_hat] -= x * 0.1
+    return weights
+
+
+# Perceptron Algorithm
+def perceptron():
+    weights = train_perceptron()
+    return predict(weights)
+
+
+# estimate Passive Aggressive weights
+def train_passive_aggressive():
+    normalized_x_train = z_score_normalization(x_train_lines, x_train_lines)
+    normalized_x_train = np.insert(normalized_x_train, 0, np.ones(normalized_x_train.shape[0]), axis=1)
+    epochs = 5
+    weights = [[0] * 6 for i in range(3)]
+    for i in range(epochs):
+        for x, y in zip(normalized_x_train, y_train_lines):
+            y_hat = np.argmax(np.dot(weights, x))
+            if y_hat != y:
+                tau = max(0, 1 - np.dot(weights[int(y)], x) + np.dot(weights[int(y_hat)], x))\
+                      / (2 * pow(np.linalg.norm(x), 2))
+                weights[int(y)] += x * tau
+                weights[int(y_hat)] -= x * tau
+    return weights
+
+
+# Passive Aggressive Algorithm
+def passive_aggressive():
+    weights = train_passive_aggressive()
+    return predict(weights)
+
+
+# estimate SVM weights
+def train_svm():
+    normalized_x_train = z_score_normalization(x_train_lines, x_train_lines)
+    normalized_x_train = np.insert(normalized_x_train, 0, np.ones(normalized_x_train.shape[0]), axis=1)
+    weights = np.array([[0] * 6 for i in range(3)])
+    epochs = 10
+    for i in range(epochs):
+        for x, y in zip(normalized_x_train, y_train_lines):
+            dot_result = np.array(np.dot(weights, x))
+            dot_result[int(y)] = float('-inf')
+            y_hat = np.argmax(dot_result)
+            tau = max(0, 1 - np.dot(weights[int(y)], x) + np.dot(weights[int(y_hat)], x))
+            eta = 0.1
+            lamda = 0.0002
+            if tau > 0:
+                weights = (1 - lamda * eta) * weights
+                weights[int(y)] = weights[int(y)] + eta * x
+                weights[int(y_hat)] = weights[int(y_hat)] - eta * x
+            else:
+                weights = (1 - lamda * eta) * weights
+    return weights
+
+
+# SVM Algorithm
+def svm():
+    weights = train_svm()
+    return predict(weights)
+
+
+knn = knn()
+perceptron = perceptron()
+passive_aggressive = passive_aggressive()
+svm = svm()
+
+size = len(x_test_lines)
+
+out_file = open(output_file, "w")
+for n in range(size):
+    out_file.write(f"knn: {knn[n]}, perceptron: {perceptron[n]}, svm: {svm[n]}, pa: {passive_aggressive[n]}\n")
+out_file.close()
+
+
---
+++ k-means/ex1.py
@@ -0,0 +1,90 @@
+# Roni Gotlib 322805029
+from matplotlib import pyplot as plt
+import numpy as np
+import sys
+
+
+image_fname, centroids_fname, out_fname = sys.argv[1], sys.argv[2], sys.argv[3]
+z = np.loadtxt(centroids_fname)  # load centroids
+
+orig_pixels = plt.imread(image_fname)
+pixels = orig_pixels.astype(float)/255
+# Reshape the image(128x128x3) into an Nx3 matrix where N = number of pixels.
+pixels = pixels.reshape(-1, 3)
+
+
+# A function that calculates the new centroids
+def findCenters(pixelsArray, centroidsArray, clusterArray, k):
+    centers = np.zeros((k, 3))
+    for n in range(k):
+        # We check the number of pixels in each cluster and sum them up
+        counter = sum = 0
+        for index, c in enumerate(clusterArray):
+            if c == n:
+                counter += 1
+                sum += pixelsArray[index]
+        # if counter != 0, divide the sum by the counter to find the new midpoint of the cluster
+        if counter != 0:
+            centers[n] = (sum / counter)
+        else:
+            # if counter == 0, the centroid remains the same
+            centers[n] = (centroidsArray[n])
+    return centers
+
+
+# A function that receives two lists and checks if they are identical
+def compareLists(list1, list2):
+    for inx in range(len(list1)):
+        for a in range(3):
+            if list1[inx][a] != list2[inx][a]:
+                return False
+    return True
+
+
+# A function that calculates the loss values
+def loss(centroids, pixels, clusters):
+    sum, count = 0, 0
+    for i, cent in enumerate(centroids):
+        for j in range(len(clusters)):
+            cluster = clusters[j]
+            if cluster == i:
+                sum += pow(pixels[j][0] - cent[0], 2) + pow(pixels[j][1] - cent[1], 2) + \
+                       pow(pixels[j][2] - cent[2], 2)
+                count += 1
+    average = sum / count
+    return average
+
+
+outFile = open(out_fname, 'w')
+cluster = np.zeros(pixels.shape[0])
+# Counter for the number of iterations of the loop
+count = 0
+# A list to save the loss values
+cost = []
+# If we get to 20 iterations, we'll stop the loop run
+while count < 20:
+    # Assign each pixel to the appropriate cluster
+    for i, pixel in enumerate(pixels):
+        minDist = -1
+        for j, centroid in enumerate(z):
+            dist = np.sqrt(pow(centroid[0] - pixel[0], 2) + pow(centroid[1] - pixel[1], 2)
+                           + pow(centroid[2] - pixel[2], 2))
+            if dist < minDist or minDist == -1:
+                minDist = dist
+                cluster[i] = j
+    # Finding the new centroids
+    new = findCenters(pixels, z, cluster, len(z)).round(4)
+    # Add the loss value to the list
+    cost.insert(len(cost), loss(new, pixels, cluster))
+    # Check if the list of new centroids is the same as the old ones
+    if compareLists(z, new):
+        outFile.write(f"[iter {count}]:{','.join([str(i) for i in new])}\n")
+        # If the new centroids are the same as the old ones, we stop the loop run
+        break
+    else:
+        outFile.write(f"[iter {count}]:{','.join([str(i) for i in new])}\n")
+        # Updating the centroids to the new ones
+        z = new
+    count += 1
+
+outFile.close()
